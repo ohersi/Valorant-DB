f4cf3d902e237a2da9b323148e4509faddbdc25e
Omar Hersi <o.hersi@yahoo.com>
2022-01-28 13:36:31 -0500

    test call to valorant-api, display data in App.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import './App.css'
 
-function App() {
+const App = () => {
+
+  const [valData, setValData] = useState([])
+
+  const fetchVal = async () => {
+    try {
+      const response = await axios.get('https://valorant-api.com/v1/agents')
+      setValData(response.data.data)
+
+    }
+    catch (error) {
+      console.error(error)
+    }
+  }
+
+  console.log(valData)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div id='main'>
+
+        <button onClick={fetchVal}>Test Valorant-API</button>
+
+        <div id="val-container">
+          {
+            valData.map(item => (
+              <div key={item.displayName}>
+                <h2>{item.isPlayableCharacter == true ?
+                  item.displayName : null}</h2>
+                <img id='val-portrait' src={item.isPlayableCharacter == true ? item.displayIcon : null} alt="" />
+                <h3>{item.isPlayableCharacter == true ? item.role?.displayName : null}</h3>
+              </div>
+            ))
+          }
+        </div>
+      </div>
+    </>
   );
 }
 
