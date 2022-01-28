7c3f19ad28dafecd80cc35e0ab7d4d7482cb1b79
Omar Hersi <o.hersi@yahoo.com>
2022-01-28 14:20:30 -0500

    added test call to Pandascore API, displaying in App.js with test styling

--- src/App.js
+++ src/App.js
@@ -2,9 +2,31 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import './App.css'
 
+console.log(process.env.REACT_APP_PANDASCORE_API_KEY)
 const App = () => {
 
+  const [matchData, setMatchData] = useState([])
   const [valData, setValData] = useState([])
+
+  const options = {
+    method: 'GET',
+    url: 'https://api.pandascore.co/valorant/matches',
+    params: { sort: '', page: '1', per_page: '10' },
+    headers: {
+      Accept: 'application/json',
+      Authorization: `Bearer ${process.env.REACT_APP_PANDASCORE_API_KEY}`
+    }
+  };
+
+  const fetchMatches = async () => {
+    try {
+      const response = await axios.request(options)
+      setMatchData(response.data);
+    }
+    catch (error) {
+      console.error(error)
+    }
+  }
 
   const fetchVal = async () => {
     try {
@@ -19,11 +41,28 @@ const App = () => {
 
   console.log(valData)
 
+  // useEffect(() => {
+  //   fetchMatches()
+  // }, []);
+
   return (
     <>
       <div id='main'>
-
+        <button onClick={fetchMatches}>Test Match API</button>
         <button onClick={fetchVal}>Test Valorant-API</button>
+
+        <div id="match-container">
+          {
+            matchData.map((matches) => (
+              <div>
+                <img src={matches.league.image_url} alt="" style={{ width: '10rem' }} />
+                <h3>{matches.league.name}</h3>
+                <h4 style={{ color: 'red' }}>{matches.name}</h4>
+                <a href={matches.official_stream_url}>Watch on Twitch</a>
+              </div>
+            ))
+          }
+        </div>
 
         <div id="val-container">
           {
@@ -37,6 +76,9 @@ const App = () => {
             ))
           }
         </div>
+
+
+
       </div>
     </>
   );
